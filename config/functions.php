58d063a5c547f6fc164e12c7ee68839a5f310c5f
Alex Stoica <alex_stoicajr@yahoo.co.uk>
2013-03-08 21:09:34 +0200

    menu is function generated.

--- config/functions.php
+++ config/functions.php
@@ -4,4 +4,37 @@
 	{
 		return isset ( $_SESSION ['user_id'] ) ;
 	}
+
+	function show_menu ( $adress )
+	{
+		$page = substr ( strrchr($adress, '/') , 1 );
+
+		echo'
+	        <div class="navbar-inner">
+	            <div class="container-fluid">
+	                <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+	                    <span class="icon-bar"></span>
+	                    <span class="icon-bar"></span>
+	                    <span class="icon-bar"></span>
+	                </button>
+	                <a class="brand" href="index.php">Warehousing</a>
+	                <div class="nav-collapse collapse">
+	                    <ul class="nav">
+	                        <li class="'.isActive ( 'index.php' , $page ).'"><a href="#">Home</a></li>
+	                        <li class=""><a href="#about">Second Page</a></li>
+	                        <li><a href="#contact">Contact</a></li>
+	                    </ul>
+	                </div>
+	                <!--/.nav-collapse -->
+	            </div>
+	        </div>';
+
+    }
+
+	function isActive ( $link , $page )
+	{
+		if ( $link == $page )
+			return 'active';
+		return '' ;
+	}
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+
+    require_once ( "config/functions.php" ) ;
+
+?>
+
 <!DOCTYPE html>
 
 <html lang="en">
@@ -49,27 +55,8 @@
 
 <body>
     <div class="navbar navbar-inverse navbar-fixed-top">
-
-        <div class="navbar-inner">
-            <div class="container-fluid">
-                <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="brand" href="#">Warehousing</a>
-                <div class="nav-collapse collapse">
-                    <ul class="nav">
-                        <li class="active"><a href="#">Home</a></li>
-                        <li><a href="#about">About</a></li>
-                        <li><a href="#contact">Contact</a></li>
-                    </ul>
-                </div>
-                <!--/.nav-collapse -->
-            </div>
-        </div>
+        <?php  echo show_menu ( $_SERVER["PHP_SELF"] ) ; ?>
     </div>
-
     <div class="container">
         <div id="tablePlace">
         </div>
