d0353a0ddf4ba22f342ae9380b01e50c6878f9da
Alex Stoica <alex_stoicajr@yahoo.co.uk>
2013-03-16 22:42:14 +0200

    removed whitespace

--- config/config.php
+++ config/config.php
@@ -4,8 +4,8 @@
 	$db_password = "" ;
 	$db_name = "warehous_lucru";
 	$db_move = "moves" ;
-	
+
 	$connection = mysql_connect ( $db_host , $db_user , $db_password ) or die ("Error connecting to DB");
 	mysql_select_db ( $db_name , $connection ) or die ( mysql_error() ) ;
-	
+
 ?>
