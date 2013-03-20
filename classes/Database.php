da88037f9e90f96d1387ebecd6581ff56a70ede2
Alex Stoica <alex_stoicajr@yahoo.co.uk>
2013-03-20 21:13:42 +0200

    added Database class as a standalone file.

---
+++ classes/Database.php
@@ -0,0 +1,28 @@
+<?php
+
+class Database {
+
+	static $db = NULL ;
+
+	public function __construct ( )
+	{
+		if ( self::$db == NULL )
+		{
+			$db_user = "root" ;
+			$db_password = "" ;
+			$db_name = "warehous_lucru" ;
+			$db_host = "localhost" ;
+			self::$db = new PDO ( 'mysql:host='.$db_host.';dbname='.$db_name , $db_user , $db_password ) ;
+		}
+	}
+
+	public function query_database ( $query , $params = array () )
+	{
+		$prepared_statement = self::$db -> prepare ($query) ;
+		$prepared_statement -> execute ( $params ) ;
+		$result = $prepared_statement ->fetchAll(PDO::FETCH_ASSOC) ;
+		return $result ;
+	}
+}
+
+?>
--- database_testing.php
+++ database_testing.php
@@ -1,35 +1,13 @@
 <?php
 
-class Database {
-
-	static $db = NULL ;
-
-	public function __construct ( )
-	{
-		if ( self::$db == NULL )
-		{
-			$db_user = "root" ;
-			$db_password = "" ;
-			$db_name = "warehous_lucru" ;
-			$db_host = "localhost" ;
-			self::$db = new PDO ( 'mysql:host='.$db_host.';dbname='.$db_name , $db_user , $db_password ) ;
-		}
-	}
-
-	public function query_database ( $query , $params )
-	{
-		$prepared_statement = self::$db -> prepare ($query) ;
-		$prepared_statement -> execute ( $params ) ;
-		$result = $prepared_statement ->fetchAll(PDO::FETCH_ASSOC) ;
-		return $result ;
-;	}
-}
+require_once ( "classes/Database.php") ;
 
 $db_conn = new Database () ;
 
-$user_id = "1" ;
-$result = $db_conn -> query_database ( "SELECT * FROM users WHERE `id`= :user_id" , array ( "user_id" => $user_id ) ) ;
+$start = "1" ;
+$final = "10" ;
+$result = $db_conn -> query_database ( "SELECT * FROM items WHERE `quantity`!= 0 AND :start<=`id` AND `id`<:final" , array ( "start" => $start , "final" => $final ) ) ;
 
 foreach ( $result as $row )
-	print_r ( $row ) ;
+	echo $row ['item_code'].'<br>';
 ?>
