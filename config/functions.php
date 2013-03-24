daae21fc3ace33b0663379ab3482b0524ec65122
Alex Stoica <alex_stoicajr@yahoo.co.uk>
2013-03-24 20:38:30 +0200

    logout button will show only on index.

--- config/functions.php
+++ config/functions.php
@@ -10,26 +10,28 @@
 		$page = substr ( strrchr($adress, '/') , 1 );
 
 		echo'
-	        <div class="navbar-inner">
-	            <div class="container-fluid">
-	                <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-	                    <span class="icon-bar"></span>
-	                    <span class="icon-bar"></span>
-	                    <span class="icon-bar"></span>
-	                </button>
-	                <a class="brand" href="index.php">Warehousing</a>
-	                <div class="nav-collapse collapse">
-	                    <ul class="nav">
-	                        <li class="'.isActive ( 'index.php' , $page ).'"><a href="index.php">Home</a></li>
-	                        <li class="'.isActive ( 'cart.php' , $page ).'"><a href="cart.php">Cart</a></li>
-	                        <li><a href="#" onclick="logout()">Logout</a></li>
-	                    </ul>
-	                </div>
-	                <!--/.nav-collapse -->
-	            </div>
-	        </div>';
+			<div class="navbar-inner">
+				<div class="container-fluid">
+					<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="brand" href="index.php">Warehousing</a>
+					<div class="nav-collapse collapse">
+						<ul class="nav">
+							<li class="'.isActive ( 'index.php' , $page ).'"><a href="index.php">Home</a></li>
+							<li class="'.isActive ( 'cart.php' , $page ).'"><a href="cart.php">Cart</a></li>';
+		if ( isActive ( 'index.php' , $page ) )
+			echo '<li><a href="#" onclick="logout()">Logout</a></li>';
 
-    }
+		echo'
+						</ul>
+					</div>
+					<!--/.nav-collapse -->
+				</div>
+			</div>';
+	}
 
 	function isActive ( $link , $page )
 	{
