dda6350517925ede2b611f358429cfceeec32c8e
Alex Stoica <alex_stoicajr@yahoo.co.uk>
2013-06-18 20:32:52 +0300

    added gitignore

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+	$pathToLibs = realpath(__DIR__.'/../');
+	set_include_path($pathToLibs . PATH_SEPARATOR  . get_include_path());
+
+	require_once ( "classes/Database.php" ) ;
+	require_once ( "classes/Item.php") ;
+	require_once ( "config/session.php") ;
+
+	$db = new Database () ;
